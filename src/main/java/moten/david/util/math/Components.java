c246412cdb61a09dd1220f5da150a8fbfb2c28ef
Dave Moten <davidmoten@gmail.com>
2013-03-25 17:02:56 +1100

    add javadoc and rename a method

--- src/main/java/moten/david/util/math/Components.java
+++ src/main/java/moten/david/util/math/Components.java
@@ -31,14 +31,13 @@ public class Components {
 		return eigenvectors.times(eigenvalues.apply(Matrix.SQUARE_ROOT));
 	}
 
-	public Components getPrincipalComponents(
-			EigenvalueThreshold threshold) {
+	public Components getPrincipalComponents(EigenvalueThreshold threshold) {
 
 		// apply min eigenvalue criterion if set
 		Components c = removeEntriesLessThanMinEigenvalue(threshold);
 
 		// apply max factors criterion if set
-		c = removeEntriesAccordingToMaxFactors(c, threshold);
+		c = removeEntriesMoreThanMaxFactors(c, threshold);
 
 		// get positive manifold
 		c = c.normalizeLoadingSigns();
@@ -47,13 +46,12 @@ public class Components {
 		return c;
 	}
 
-	public Components removeEntriesAccordingToMaxFactors(Components c,
+	public Components removeEntriesMoreThanMaxFactors(Components c,
 			EigenvalueThreshold threshold) {
 
 		if (threshold.getPrincipalFactorCriterion().equals(
 				PrincipalFactorCriterion.MAX_FACTORS)
-				&& c.getEigenvalues().rowCount() > threshold
-						.getMaxFactors()) {
+				&& c.getEigenvalues().rowCount() > threshold.getMaxFactors()) {
 
 			final Vector eigenvaluesVector = c.getEigenvaluesVector();
 			Matrix eValues = c.getEigenvalues();
@@ -63,9 +61,11 @@ public class Components {
 			int numToDelete = eigenvaluesVector.rowCount()
 					- threshold.getMaxFactors();
 
+			// get the indices of the eigenvalues in ascending order
 			List<Integer> ordered = eigenvaluesVector
 					.getOrderedIndicesByAbsoluteValue(true);
 
+			// get the indices that we are going to delete
 			List<Integer> removeThese = ordered.subList(0, numToDelete);
 
 			// now sort the indices to remove because we must remove
@@ -75,7 +75,10 @@ public class Components {
 			// remove the rows/columns from the bottom/right first
 			for (int j = removeThese.size() - 1; j >= 0; j--) {
 				Integer row = removeThese.get(j);
+				// remove the factor marked for deletion from the diagonal
+				// matrix of eigenvalues
 				eValues = eValues.removeRow(row).removeColumn(row);
+				// remove the factor marked for deletion from the eigenvectors
 				eVectors = eVectors.removeColumn(row);
 			}
 			return new Components(eVectors, eValues);
