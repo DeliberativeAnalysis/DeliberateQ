fda76c5e9d2ac673ede9fafbd05aaee39d22b087
Dave Moten <davidmoten@gmail.com>
2018-05-30 16:15:49 +1000

    add release.sh script

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+set -x
+git checkout master
+git pull
+mvn versions:set -DnewVersion=$1
+mvn versions:commit
+mvn clean install
+git commit -am "[release] prepare release $1"
+git push
+git tag -a $1 -m "release version $1"
+git push origin $1
+mvn versions:set -DnewVersion=$1.1-SNAPSHOT
+mvn versions:commit
+git commit -am "[release] prepare for next development iteration"
+git push
+git checkout $1
+mvn clean install
+echo ==================================================
+echo Now deploy target/*-with-dependencies.jar to
+echo github release page
+echo ==================================================
+
