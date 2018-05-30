b2b19083e85a5b2bb1bf3cf85661e896546020fa
Dave Moten <davidmoten@gmail.com>
2018-05-30 14:58:54 +1000

    cleanup, remove unused class MatrixTestData

--- src/main/java/com/github/deliberateq/util/math/RegressionIntervalFunction.java
+++ src/main/java/com/github/deliberateq/util/math/RegressionIntervalFunction.java
@@ -2,30 +2,32 @@ package com.github.deliberateq.util.math;
 
 import org.apache.commons.math3.distribution.TDistribution;
 
+import com.google.common.base.Preconditions;
+
 public final class RegressionIntervalFunction implements Function {
 
-	private final Vector v1;
-	private final boolean singleResponse;
-	private TDistribution tDistribution;
-	private final double sumSquares;
-	private final double mean;
-	private final double sum;
+    private final Vector v1;
+    private final boolean singleResponse;
+    private final TDistribution tDistribution;
+    private final double sumSquares;
+    private final double mean;
+    private final double sum;
 
-	public RegressionIntervalFunction(Vector v1, boolean singleResponse) {
-		this.v1 = v1;
-		this.singleResponse = singleResponse;
-		if (v1.size() > 0)
-			this.tDistribution = new TDistribution(v1.size());
-		this.sumSquares = v1.getSumSquares();
-		this.mean = v1.getMean();
-		this.sum = v1.getSum();
-	}
+    public RegressionIntervalFunction(Vector v1, boolean singleResponse) {
+        Preconditions.checkArgument(v1.size() > 0, "Vector v1 should not be empty");
+        this.v1 = v1;
+        this.singleResponse = singleResponse;
+        this.tDistribution = new TDistribution(v1.size());
+        this.sumSquares = v1.getSumSquares();
+        this.mean = v1.getMean();
+        this.sum = v1.getSum();
+    }
 
-	@Override
-	public double f(double x) {
-		double s = Math.sqrt(v1.getResiduals().getSumSquares() / (v1.size() - 2));
-		double t95 = tDistribution.inverseCumulativeProbability(0.95);
-		return t95 * s * Math.sqrt((singleResponse ? 1 : 0) + 1.0 / v1.size()
-				+ Math.pow(x - mean, 2) / (sumSquares - Math.pow(sum, 2) / v1.size()));
-	}
+    @Override
+    public double f(double x) {
+        double s = Math.sqrt(v1.getResiduals().getSumSquares() / (v1.size() - 2));
+        double t95 = tDistribution.inverseCumulativeProbability(0.95);
+        return t95 * s * Math.sqrt((singleResponse ? 1 : 0) + 1.0 / v1.size()
+                + Math.pow(x - mean, 2) / (sumSquares - Math.pow(sum, 2) / v1.size()));
+    }
 }
